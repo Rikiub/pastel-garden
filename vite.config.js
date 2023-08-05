67af79ac826ae46f659d8601f3dcbae8622d75c9
Rikiub <rikiub.dev@gmail.com>
2023-08-05 16:02:05 -0400

    directory structure changed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { glob } from "glob"
+import path from "path"
 
 export default defineConfig ({
-	base: '/'
-})
+    root: path.join(__dirname, "src"),
+    build: {
+        outDir: path.join(__dirname, "dist"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: glob.sync(path.resolve(__dirname, "src", "**/*.html")),
+        }
+    }
+});
